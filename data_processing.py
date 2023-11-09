7c2e4401db1a6233c623f65ed03aa5f790080f15
Pattharamon Dumrongkittikule <pattharamon.d@ku.th>
2023-11-09 09:36:39 +0700

    3 tasks

--- data_processing.py
+++ data_processing.py
@@ -135,6 +135,9 @@ print(f"Average number of passes made by midfielders: {midfielder.aggregate(lamb
 
 print()
 
+# task 4
+print("The average fare paid by passengers in the first class versus in the third class")
+
 """
 print("Test filter: only filtering out cities in Italy") 
 my_table1_filtered = my_table1.filter(lambda x: x['country'] == 'Italy')
