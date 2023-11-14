afc512bd88a3183bb19b3f92d20a89b7891b86c5
Pattharamon Dumrongkittikule <pattharamon.d@ku.th>
2023-11-14 22:36:03 +0700

    add: Find the total number of male passengers embarked at Southampton

--- combination_gen.py
+++ combination_gen.py
@@ -13,7 +13,6 @@ def gen_comb_list(list_set):
             gen_comb_list([[1, 2, 3], [4, 5]]) returns [[1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5]]
             gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]) returns [[1, 4, 6], [2, 4, 6], [3, 4, 6], [1, 5, 6], [2, 5, 6], [3, 5, 6], [1, 4, 7], [2, 4, 7], [3, 4, 7], [1, 5, 7], [2, 5, 7], [3, 5, 7], [1, 4, 8], [2, 4, 8], [3, 4, 8], [1, 5, 8], [2, 5, 8], [3, 5, 8]]
     '''
-
     if len(list_set) == 1:
         start_list = []
         for item in list_set[0]:
@@ -27,7 +26,4 @@ def gen_comb_list(list_set):
             temp_item.append(val)
             start_list.append(temp_item)
     return start_list
-
-
-
 
