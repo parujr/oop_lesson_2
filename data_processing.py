4a40eb17edbbb2b70953687c8872c89a1def7293
Pattharamon Dumrongkittikule <pattharamon.d@ku.th>
2023-11-14 20:37:42 +0700

    add: Find the total number of male passengers embarked at Southampton

--- data_processing.py
+++ data_processing.py
@@ -70,13 +70,25 @@ class Table:
             if condition(item1):
                 filtered_table.table.append(item1)
         return filtered_table
-    
+
+    def __is_float(self, element):
+        if element is None:
+            return False
+        try:
+            float(element)
+            return True
+        except ValueError:
+            return False
+
     def aggregate(self, function, aggregation_key):
         temps = []
         for item1 in self.table:
-            temps.append(float(item1[aggregation_key]))
+            if self.__is_float(item1[aggregation_key]):
+                temps.append(float(item1[aggregation_key]))
+            else:
+                temps.append(item1[aggregation_key])
         return function(temps)
-    
+
     def select(self, attributes_list):
         temps = []
         for item1 in self.table:
